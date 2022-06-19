d50a1c1814551aaf9b8a7b8ef0273c090a5b4765
Oybek <oybekjonxayrullayevich@gmail.com>
2022-06-19 10:34:56 +0500

    redux yozdm

--- src/redux/redux.js
+++ src/redux/redux.js
@@ -1,38 +1,66 @@
 import { combineReducers, createStore } from "redux";
 
-export const dec = () => {
+// export const dec = () => {
+//   return {
+//     type: "DEC",
+//   };
+// };
+// export const res = () => {
+//   return {
+//     type: "RES",
+//   };
+// };
+// export const inc = () => {
+//   return {
+//     type: "INC",
+//   };
+// };
+
+// const initialState = 0;
+
+// const reducers = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "INC":
+//       return state - 1;
+//     case "DEC":
+//       return state + 1;
+//     case "RES":
+//       return (state = 0);
+//     default:
+//       return state;
+//   }git
+// };
+
+// export const store = createStore(
+//   reducers,
+//   initialState,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+const qush = () => {
   return {
-    type: "DEC",
+    type: "QUSH",
   };
 };
-export const res = () => {
+const nol = () => {
   return {
-    type: "RES",
-  };
-};
-export const inc = () => {
-  return {
-    type: "INC",
+    type: "NOL",
   };
 };
 
+const ayir = () => {
+  return {
+    type: "AYIR",
+  };
+};
 const initialState = 0;
 
 const reducers = (state = initialState, action) => {
   switch (action.type) {
-    case "INC":
-      return state - 1;
-    case "DEC":
+    case "QUSH":
       return state + 1;
-    case "RES":
+    case "AYIR":
+      return state - 1;
+    case "NOL":
       return (state = 0);
-    default:
-      return state;
-  }git
+  }
 };
-
-export const store = createStore(
-  reducers,
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
